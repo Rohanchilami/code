1fd2ab1628a0905ef5886bfc1fe215de1d63ec8a
RohanC-0521 <rohanchilmai21@gmail.com>
2025-02-10 10:28:17 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+$('#hero-slider').owlCarousel({
+    loop:true,
+    margin:0,
+    items:1,
+    nav:true,
+    responsive:{
+        0:{
+            
+        },
+        600:{
+            
+        },
+        1000:{
+            
+        }
+    }
+})
+
+});
